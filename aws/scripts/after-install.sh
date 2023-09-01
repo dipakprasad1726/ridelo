4a690af85f78a8cebf5072e5c48b343c24f6c22c
Dipak Kumar Prasad <dipakprasad53@gmail.com>
2023-09-02 04:41:48 +0530

    Add Base

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://arn:aws:s3:::codedeploystack-webappdeploymentbucket-12fnmzouqydsu/ridelo.war /usr/local/tomcat9/webapps/ridelo.war
+aws s3 cp codedeploystack-webappdeploymentbucket-12fnmzouqydsu/ridelo.war /usr/local/tomcat9/webapps/ridelo.war
 
 
 # Ensure the ownership permissions are correct.
