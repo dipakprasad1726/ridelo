d583ed35de5b892d6a5af67f6a5ccc5053ffdaea
Dipak Kumar Prasad <dipakprasad53@gmail.com>
2023-09-02 05:07:18 +0530

    Add Base

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-12fnmzouqydsu/ridelo.war /usr/local/tomcat9/webapps/ridelo.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-12fnmzouqydsu/spring-boot-hello-world-example.war /usr/local/tomcat9/webapps/spring-boot-hello-world-example.war
 
 
 # Ensure the ownership permissions are correct.
