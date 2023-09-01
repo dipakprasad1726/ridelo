824f76bff474678cb58d426934cc3666a25ab4d3
Dipak Kumar Prasad <dipakprasad53@gmail.com>
2023-09-02 04:14:25 +0530

    Add Base

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -2,9 +2,10 @@ package com.helloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/hi")
 public class HelloWorldController 
 {
-@RequestMapping("/")
+@GetMapping("/")
 public String hello() 
 {
 return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
