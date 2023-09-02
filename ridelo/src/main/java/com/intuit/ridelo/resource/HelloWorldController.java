7cc870cf05bfe22ffe30e784c5aa8d6aef737271
Dipak Kumar Prasad <dipakprasad53@gmail.com>
2023-09-02 23:37:51 +0530

    Add Base

---
+++ ridelo/src/main/java/com/intuit/ridelo/resource/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.helloworld.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("api/")
+public class HelloWorldController 
+{
+    @GetMapping("/hi")
+    public String hello()
+{
+return "<h1> Congratulations Dipak. You have successfully deployed the sample Spring Boot Application. </h1>";
+}
+}
---
+++ ridelo/src/test/java/com/intuit/ridelo/RideloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.ridelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
