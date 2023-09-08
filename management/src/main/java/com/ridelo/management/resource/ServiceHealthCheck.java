e741cbedc8072025dee7e017bcb3c928e76072ec
Dipak Kumar Prasad <dipakprasad53@gmail.com>
2023-09-08 08:24:25 +0530

    Update

--- management/src/main/java/com/ridelo/management/resource/ServiceHealthCheck.java
+++ management/src/main/java/com/ridelo/management/resource/ServiceHealthCheck.java
@@ -2,13 +2,15 @@ package com.ridelo.management.resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/")
 public class ServiceHealthCheck {
 
+    @GetMapping
     public ResponseEntity<Object> healthCheck(){
         return new ResponseEntity<>("Welcome to Ridelo service. Service is running fine..", HttpStatus.ACCEPTED);
     }
